5650eaed270931f8f917d6c6673cd5cccf817e65
ddc007 <391565320@qq.com>
2018-02-27 11:30:34 +0800

    完成自动去除重复头文件的功能

--- FastProperty-Xcode/FastProperty/SourceEditorCommand.m
+++ FastProperty-Xcode/FastProperty/SourceEditorCommand.m
@@ -10,7 +10,6 @@
 #import "NSString+fastProperty.h"
 
 @implementation SourceEditorCommand
-
 - (void)performCommandWithInvocation:(XCSourceEditorCommandInvocation *)invocation completionHandler:(void (^)(NSError * _Nullable nilOrError))completionHandler
 {
     // 选中去除重复头文件选项
@@ -24,7 +23,6 @@
     
     completionHandler(nil);
 }
-
 
 // 自动添加属性
 - (void)audoAddClassProperty:(XCSourceEditorCommandInvocation *)invocation{
@@ -72,8 +70,6 @@
             
             // 赋值属性
             
-            
-            
         }
     }
     
@@ -81,57 +77,45 @@
 
 // 移除重复头文件
 - (void)removeDuplicateImportHeaders:(XCSourceEditorCommandInvocation *)invocation{
-    //headerDict存放文本中所有的头文件
-    NSMutableDictionary <NSString*, NSNumber *> *headerDict = [NSMutableDictionary dictionary];
-    //willCheckDict存放将要删除的头文件
-    NSMutableDictionary <NSNumber*, NSString *> *willCheckDict = [NSMutableDictionary dictionary];
+    //headerDict存放文本中需要导入的头文件
+    NSMutableArray  *headerArr = [NSMutableArray array];
+    //willCheckDict存放将要删除的重复的头文件
+    NSMutableArray  *willCheckArr = [NSMutableArray array];
     
     //遍历编辑器每一行
     for (int idx = 0; idx < invocation.buffer.lines.count; idx++) {
         NSString *lineCode = invocation.buffer.lines[idx];
-        //若willCheckDict文件不为空，则进行是否使用了该头文件的判断
-        if (willCheckDict.count > 0)
-        {
-            [willCheckDict enumerateKeysAndObjectsUsingBlock:^(NSNumber * _Nonnull key, NSString * _Nonnull checkString, BOOL * _Nonnull stop) {
-                if ([lineCode containsString:checkString]) {
-                    if (![lineCode containsString:@"#import"]) {
-                        if ([headerDict[checkString] isEqualToNumber: @1]) {
-                            //若使用了该头文件，则从willCheckDict字典中提出该项
-                            [willCheckDict removeObjectForKey:key];
-                            //同时设置该头文件已经检查过，若后续仍出现该头文件，则可以进行删除
-                            headerDict[checkString] = @0;
-                        }
+        lineCode = [self stringWithRemoveTrimmingCharacters:lineCode];
+        if ([lineCode containsString:@"#import"]) {
+            // 如果包含import 说明是头文件引入
+            if ([[lineCode substringWithRange:NSMakeRange(0, 7)] isEqualToString:@"#import"]) {
+                //开始引入
+                NSDictionary *header = @{@"header":lineCode,@"line":@(idx)};
+                for (NSDictionary *dict in headerArr) {
+                    if ([dict[@"header"] isEqualToString:lineCode]) {
+                        [willCheckArr addObject:header];
                     }
                 }
-            }];
-        }
-        
-        //检测代码是否含有#import为头文件标志；+号我们认为是类扩展的标志
-        if ([lineCode containsString:@"#import"] && ![lineCode containsString:@"+"]) {
-            //解析获取类名
-            NSRange range1 = [lineCode rangeOfString:@"\""];
-            NSRange range2 = [lineCode rangeOfString:@"\"" options:NSBackwardsSearch];
-            NSRange zeroRange = NSMakeRange(0, 0);
-            
-            if (!(NSEqualRanges(range1, zeroRange) || NSEqualRanges(range2, zeroRange))) {
-                NSRange findRange = NSMakeRange(range1.location + 1, range2.location - range1.location - 3);
-                NSString *classString = [lineCode substringWithRange:findRange];
-                willCheckDict[@(idx)] = classString;
-                headerDict[classString] = @1;
+                
+                if (![willCheckArr containsObject:header]) {
+                    [headerArr addObject:header];
+                }
             }
         }
     }
-    
-    
-    //取出需要删除的行
-    NSMutableIndexSet *index = [NSMutableIndexSet indexSet];
-    [willCheckDict.allKeys enumerateObjectsUsingBlock:^(NSNumber * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        [index addIndex:obj.integerValue];
-        
-    }];
-    
+    NSMutableIndexSet *indexs = [NSMutableIndexSet indexSet];
+    for (NSDictionary *dict in willCheckArr) {
+        NSNumber *index = dict[@"line"];
+        [indexs addIndex:index.integerValue];
+    }
+  
     //删除不符合条件的行
-    [invocation.buffer.lines removeObjectsAtIndexes:index];
+    [invocation.buffer.lines removeObjectsAtIndexes:indexs];
+    
+}
+
+- (NSString *)stringWithRemoveTrimmingCharacters:(NSString *)string{
+    return [string stringByReplacingOccurrencesOfString:@" " withString:@""];
 }
 
 
